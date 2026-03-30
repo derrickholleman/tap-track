ea924ed8e2e09162069b0afd8f4081e301255c71
Derrick Holleman <derrickholleman@gmail.com>
2026-03-29 21:08:24 -0500

    test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
 	await page.goto('/');
 
-	await expect(page.getByText('Hello World')).toBeVisible();
+	await expect(page.getByText('Hello Worl')).toBeVisible();
 });
